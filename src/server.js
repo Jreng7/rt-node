0403f6f7faaaca839025121fd73ec3c5529b9224
Jreng7 <jr.waydev@gmail.com>
2024-09-15 19:46:51 -0300

    create paste streams and fundamentals.js

--- src/server.js
+++ src/server.js
@@ -4,21 +4,41 @@ import http from 'node:http' // ESModules => import/Export
 // req = Requisição, ou seja, está chamando o nosso servidor.
 // res = Response (Resposta) Está devolvendo ao front end a resposta pedida ao back end
 
+// Stateful - Stateless
+
+// JSON - JavaScript Object Notation
+
+// Cabeçalhos (Requisição/Resposta) => Metadados
+
+// HTTP Status Code
+
+
+const users = []
+
 const server = http.createServer((req, res) => {
   const { method, url } = req
 
   if ( method === 'GET' && url === '/users') {
 
     // Early return 
-    return res.end('Listagem de usuários')
+    return res
+    .setHeader('Content-type', 'aplication/json')
+    .end(JSON.stringify(users))
   }
 
   if ( method === 'POST' && url === '/users') {
-    return res.end('Criação de Usuário.')
+
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@gmail.com'
+    })
+
+    return res.writeHead(201).end()
   }
 
 
-  return res.end('Curso Nodejs pela rocketseat')
+  return res.writeHead(404).end('Not Found')
 })
 
 server.listen(3333)
