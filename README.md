77cad69e8eebc89f67eae3a782e027f004a1d5a8
Jreng7 <jr.waydev@gmail.com>
2024-09-14 21:18:21 -0300

    change CommonJs from ESModule

--- README.md
+++ README.md
@@ -2,5 +2,3 @@
 Curso na plataforma da Rocketseat - Módulo Node.
 ### 06/09/2024 - Iniciando com Node.js
 * Criando um projeto node.js
-
-Sem user framework
--- src/server.js
+++ src/server.js
@@ -1,2 +1,8 @@
-const http = require("http") // Common JS 
+// Forma antiga => const http = require('http') = Usava-se o CommonJS
+import http from 'http' // ESModules => import/Export
 
+const server = http.createServer((req, res) => {
+  res.end('server online')
+})
+
+server.listen(3333)
