03bf1a57a264cc6b20f1cafa01ea78a33c013d0a
Jreng7 <jr.waydev@gmail.com>
2024-09-14 23:27:10 -0300

    add info readme and put info in server.js

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 Curso na plataforma da Rocketseat - Módulo Node.
 ### 06/09/2024 - Iniciando com Node.js
 * Criando um projeto node.js
+
+---------------------- // --------------------------
+
+// Forma antiga => const http = require('http') = Usava-se o CommonJS
+
+{ import http from 'node:http' // ESModules => import/Export }
+
+// req = Requisição, ou seja, está chamando o nosso servidor.
+// res = Response (Resposta) Está devolvendo ao front end a resposta pedida ao back end
--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 // Forma antiga => const http = require('http') = Usava-se o CommonJS
 import http from 'node:http' // ESModules => import/Export
 
-const server = http.createServer((req, res) => {
-  res.end('server online')
+// req = Requisição, ou seja, está chamando o nosso servidor.
+// res = Response (Resposta) Está devolvendo ao front end a resposta pedida ao back end
+
+const server = http.createServer((request, response) => {
+  response.end('server online')
 })
 
 server.listen(3333)
