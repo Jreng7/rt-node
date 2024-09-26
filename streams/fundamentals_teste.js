aee16893960cc54beeb5948265463a28c61dd16e
Jreng7 <jr.waydev@gmail.com>
2024-09-26 16:14:09 -0300

    refazendo fundamentals_teste.js para fixar conteúdo.

--- streams/fundamentals_teste.js
+++ streams/fundamentals_teste.js
@@ -1,19 +1,19 @@
 // Tentar replicar a aula sem olhar, para ver se entendi.
 
-import { Readable, Writable, Transform } from 'node:stream'
+import { Readable, Writable } from 'node:stream'
 
 class One extends Readable {
 
   numero = 1
 
   _read(){
+
     const soma = this.numero++
 
     setTimeout(() => {
-      if(soma > 100){
+      if(soma > 100) {
         this.push(null)
       }
-  
       if(soma < 100) {
   
         const buf = Buffer.from(soma.toString())
@@ -21,23 +21,15 @@ class One extends Readable {
         this.push(buf)
       }
     }, 1000)
+
   }
 }
-
+  
 class OneTen extends Writable {
-  _write(chunk, encoding, callback) {
+  _write(chunk, encoding, callback){
     console.log(Number(chunk.toString()) * 10)
     callback()
   }
 }
 
-class OneTenLess extends Transform {
-  _transform(chunk, encoding, callback){
-    const reverso = Number(chunk.toString()) * -1
-    callback(null, String(reverso))
-  }
-}
-
-new One()
-  .pipe(new OneTenLess())
-  .pipe(new OneTen())
+new One().pipe(new OneTen())
