150531c8c4c0c9df7c09c0e5d0313b8c2bdd6671
Jreng7 <jr.waydev@gmail.com>
2024-09-25 21:50:15 -0300

    criado segundo arquivo com nome fundamental para rever assunto de stream (Readable, Writable e Transform)

---
+++ streams/fundamental.js
@@ -0,0 +1,36 @@
+// Tentar replicar a aula sem olhar, para ver se entendi.
+
+
+import { Readable, Writable, Transform } from 'node:stream'
+
+
+class One extends Readable {
+
+  numero = 1
+
+  _read(){
+    const soma = this.numero++
+
+    setTimeout(() => {
+      if(soma > 100){
+        this.push(null)
+      }
+  
+      if(soma < 100) {
+  
+        const buf = Buffer.from(soma.toString())
+  
+        this.push(buf)
+      }
+    }, 1000)
+  }
+}
+
+class OneTen extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10)
+    callback()
+  }
+}
+
+new One().pipe(new OneTen())
