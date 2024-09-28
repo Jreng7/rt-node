d2761b21af2c8e38ef92db01a7faa9597d5218a9
Jreng7 <jr.waydev@gmail.com>
2024-09-27 21:14:37 -0300

    alterações dados

--- streams/fake-upload-to-http-stream.js
+++ streams/fake-upload-to-http-stream.js
@@ -1,2 +1,27 @@
+import { Readable } from 'node:stream'
+
+class One extends Readable {
+  
+  num = 1
+  
+  _read(){
+    const soma = this.num++
+
+    setTimeout(() => {
+      if(soma > 20){
+        this.push(null)
+      } else if(soma < 20) {
+        const buf = Buffer.from(soma.toString())
+
+        this.push(buf)
+      }
+    }, 1000)
+  }
+}
 
 
+fetch('http://localhost:1234', {
+  method: 'POST',
+  body: new One(),
+  duplex: 'half'
+}) 
--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -1,2 +1,19 @@
 
-import 
+import http from 'node:http'
+import { Transform } from 'node:stream'
+
+class Negativo extends Transform {
+  _transform(chunk, encoding, callback) {
+    const dado = Number(chunk.toString()) * -1
+
+    callback(null, dado.toString())
+  }
+}
+
+const server = http.createServer((req, res) => {
+
+  return req.pipe(new Negativo).pipe(res)
+
+})
+
+server.listen(1234)
