cec79faed484dd1602b22c2e8406cfa33e913212
Jreng7 <jr.waydev@gmail.com>
2024-09-29 20:33:30 -0300

    tudo ok até o momento, porém, agora vou para stream server

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -3,3 +3,40 @@
 //process.stdin
 // .pipe(process.stdout)
 
+import { Readable, Writable, Transform } from 'node:stream'
+
+class One extends Readable {
+  
+  num = 1
+
+  _read(){
+    const numero = this.num++
+
+    setTimeout(() => {
+      if(numero > 10) {
+        this.push(null)
+      } else if (numero < 10) {
+        const buf = Buffer.from(numero.toString())
+        this.push(buf)
+      }
+    }, 1000)
+  }
+}
+
+class OneNegative extends Transform {
+  _transform(chunk, encoding, callback){
+    const results = Number(chunk.toString()) * -1
+    callback(null, String(results))
+  }
+}
+
+class OneTen extends Writable {
+  _write(chunk, encoding, callback){
+    console.log(chunk.toString() * 10)
+    callback()
+  }
+}
+
+new One()
+  .pipe(new OneNegative())
+  .pipe(new OneTen())
