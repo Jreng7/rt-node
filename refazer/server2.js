4a0d6ac11b1380e4a624d66890b96d472d6a589c
Jreng7 <jr.waydev@gmail.com>
2024-10-06 23:03:42 -0300

    mudança font etc...

--- refazer/server2.js
+++ refazer/server2.js
@@ -2,7 +2,12 @@ import http from 'node:http'
 
 const server = http.createServer((req, res) => {
 
+  const { method, url } = req
+  
+  if (method === 'GET')
+    
   return res.end('Nde.js Rocketseat')
+  
 
 })
 
