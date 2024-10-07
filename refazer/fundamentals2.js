9ca7c04244118a027d55b3ac9ec7e912e1b8cec7
Jreng7 <jr.waydev@gmail.com>
2024-10-07 14:25:04 -0300

    Refazendo aulas node.js para fixar conteudo.

---
+++ refazer/fundamentals2.js
@@ -0,0 +1,9 @@
+// process.stdout.write('Digite algo: ')
+
+// process.stdin.on('data', (infoDoWrite) => {
+//   const resposta = infoDoWrite.toString().trim()
+//   process.stdout.write(`Sua resposta foi ${resposta} `)
+// })
+
+import { Readable, Writable, Transform } from 'node:stream'
+
--- refazer/server2.js
+++ refazer/server2.js
@@ -1,12 +1,33 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((req, res) => {
 
   const { method, url } = req
   
-  if (method === 'GET')
-    
-  return res.end('Nde.js Rocketseat')
+  if (method === 'GET' && url === '/users') {
+    return res
+    .setHeader('Content-type', 'aplication/json')
+    .end(JSON.stringify(users))
+  }
+   
+  if (method === 'POST' && url === '/users') {
+
+    users.push({
+      id: 1,
+      name: 'john doe',
+      email: 'johndoe@gmail.com'
+    })
+
+    return res
+    .writeHead(201)
+    .end('Criação de usuário com sucesso!')
+  }
+
+  return res
+  .writeHead(404)
+  .end('Not Found')
   
 
 })
