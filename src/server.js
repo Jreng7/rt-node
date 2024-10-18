996f41591e468cbaba0f364336fe9e95b874afda
Jreng7 <jr.waydev@gmail.com>
2024-10-18 13:29:09 -0300

    trabalhando com middlewares

--- src/server.js
+++ src/server.js
@@ -13,6 +13,7 @@
 // HTTP Status Code
 
 import http from 'node:http'
+import { json } from './middlewares/json'
 
 const users = []
 
@@ -20,17 +21,7 @@ const server = http.createServer(async (req, res) => {
 
   const { method, url } = req
 
-  const buffers = []
-
-  for await (const chunk of req) {
-    buffers.push(chunk)
-  }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString())
-  } catch {
-    req.body = null
-  }
+  await json(req, res)
 
   if ( method === 'GET' && url === '/users') {
 
