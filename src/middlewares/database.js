d93d66f29958e594f6011043d78988090dcac59b
Jreng7 <jr.waydev@gmail.com>
2024-10-18 20:49:42 -0300

    recriando database.js

--- src/middlewares/database.js
+++ src/middlewares/database.js
@@ -1,18 +1,20 @@
 export class Database {
-
+  
   database = {}
 
   select(table) {
     const data = this.database[table] ?? []
-
-    return data
-
+    return data;
   }
 
   insert(table, data) {
-    
-  }
+    if(Array.isArray(this.database[table])){
+      this.database[table].push(data)
+    } else {
+      this.database[table] = [data]
+    }
 
-  
+    return data;
+  }
 
 }
