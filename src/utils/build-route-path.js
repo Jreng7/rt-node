faea2b85ef6d88932e5c9428dd187cc5ad676074
Jreng7 <jr.waydev@gmail.com>
2024-12-08 22:18:02 -0300

    const regexParams

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -3,4 +3,6 @@ export function buildRoutePath(path){
   const routeParametersPath = /:([a-zA-Z]+)/g
   const pathWithParams = path.replaceAll(routeParametersPath, /([a-z0-9\-_]+)/g
 
+  const regexParams = new RegExp()
+
 )}
