7825ffe7571bb9a17a3dd58556959b381344695f
Jreng7 <jr.waydev@gmail.com>
2024-12-08 22:23:44 -0300

    spred com params

--- src/server.js
+++ src/server.js
@@ -34,7 +34,9 @@ const server = http.createServer(async (req, res) => {
 
   if (route) {
 
-    const routeParams = req.url.match()
+    const routeParams = req.url.match(route.path)
+
+    const params = { ...routeParams.groups }
 
     return route.handler(req, res)
   }
